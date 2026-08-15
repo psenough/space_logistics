8353012ae28ee3bf98e3fadb0f6e75910c189ea5
Filipe Cruz <psenough@gmail.com>
2026-08-15 22:48:39 +0100

    sidetunnel shimmer sprites

---
+++ gfx/TunnelScene_extra/Shipsmall_02-1.png.lua
@@ -0,0 +1,13 @@
+-- title:  C:\Users\Utilizador\AppData\Roaming\com.nesbox.tic\TIC-80\space_logistics\gfx\TunnelScene_extra\\Shipsmall_02-1.png
+-- author: TicPanel
+-- script: lua
+
+local pal = "1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57"
+
+ -- No palette-routine found!
+
+local st = "A5E3D2A19B3DC2A21DM3E6MMEECA5HHNND3C8NNC2A3HGGHOD3C8NNC2A2HGGFHNOP8C2NNC2AAHGGFFHN3O6PCNNC3AHGGFFP13B3CCAHGGFFPJ3P8B5AAHGFFP12B6A2HGGPJ3P6B6A5HPJ3P5B6A6"
+local sx = 27
+local sy = 12
+
+ -- No hexdata-routine found!
---
+++ gfx/TunnelScene_extra/Shipsmall_02-4.png.lua
@@ -0,0 +1,13 @@
+-- title:  C:\Users\Utilizador\AppData\Roaming\com.nesbox.tic\TIC-80\space_logistics\gfx\TunnelScene_extra\\Shipsmall_02-4.png
+-- author: TicPanel
+-- script: lua
+
+local pal = "1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57"
+
+ -- No palette-routine found!
+
+local st = "A5D3E2A19B3CD2A21CE2M3E3MMEECA5HHNNC3D3C4NNC2A3HGGHOC3D3C4NNC2A2HGGFHOOP8C2NNC2AAHGGFFHO3N3O2PCNNC3AHGGFFP13B3CCAHGGFFP4J4P3B5AAHGFFP12B6A2HGGP4J4PPB6A5HP4J3PPB6A6"
+local sx = 27
+local sy = 12
+
+ -- No hexdata-routine found!

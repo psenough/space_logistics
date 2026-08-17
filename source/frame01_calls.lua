f343badc481665df17538e56a607182a57f3f4af
Filipe Cruz <psenough@gmail.com>
2026-08-18 00:45:27 +0100

    sync touches to F01

--- source/frame01_calls.lua
+++ source/frame01_calls.lua
@@ -67,10 +67,10 @@ end
 
 function Frame01(tt, demoBeats, somaticState, sceneTiming)
 	
-	local dooropen=18000
+	local dooropen=20000
 	local t = sceneTiming.demoMillis
 	local sceneX=t/105
-	local sceneY=-t/65
+	local sceneY=-t/75
 	if sceneX > 220 then sceneX = 220 end
 	if sceneY < -310 then sceneY = -310 end
 
@@ -83,8 +83,8 @@ function Frame01(tt, demoBeats, somaticState, sceneTiming)
 
 	local posGateX=310-sceneX
 	local posGateY=-310-sceneY
-	local posShipX=65-sceneX+t/66
-	local posShipY=50+math.sin(t/8000)*2-sceneY-t/65
+	local posShipX=65-sceneX+t/76
+	local posShipY=50+math.sin(t/8000)*2-sceneY-t/75
 	tri(posGateX-40,posGateY,240,0,240,136,0)
 	drawSprite("F1_BgDither",posGateX-40,posGateY)
 	drawDoorOpenAnim(t,dooropen,dooropen+1600,posGateX+53,posGateY+22)
@@ -99,7 +99,6 @@ function Frame01(tt, demoBeats, somaticState, sceneTiming)
 	if (t<(dooropen+200)) then
 		drawSprite("F1_Ship01",posShipX,posShipY)
 	else
-		--drawSpriteD("Ship01","Ship02",posShipX,posShipY)
 		drawSprite("F1_Ship02",posShipX,posShipY)
 	end
 	--else
@@ -112,20 +111,38 @@ function Frame01(tt, demoBeats, somaticState, sceneTiming)
 	circ(posShipX+9,23+posShipY,math.random(3),math.random(3)+1)
 	circ(posShipX+7,25+posShipY,math.random(2),math.random(2)+1)
 
-	-- clip right
-	--rect(posGateX+102,0,100,136,0)
-	-- clip top of gate
-	--tri(posGateX+10,posGateY, 240,0, 240,posGateY+62, 0)
+	-- mask the sprite entereing the hangar
 	drawSprite("F1_GateMask",posGateX-39,posGateY)
 
-	--if math.random() > 0.5 then	drawSprite("Logo",10,104) end
 
-	drawSprite("F1_Logo02",12,74)
-	drawSprite("F1_Logo",12,104)
-	--for i=74,140 do
-	--	if math.sin(time()/500+i)*math.sin(time()/400+i*3) > 0 then line(0,i,100,i,0) end
-	--end
-	drawSprite("F1_LogoBackdrop",0,4)
+	local rowmatch = {
+	{0,4},{0,5},{0,7},{0,10},{0,11},{0,12},{0,17},{0,22},{0,23},{0,24},{0,28},{0,29},{0,31},{0,34},{0,35},{0,36},{0,37},{0,39},{0,42},{0,43},{0,44},{0,48},{0,49},{0,51},{0,54},{0,55},{0,56},{0,60},{0,61},{0,63},
+	{1,0},{1,1},{1,3},{1,6},{1,7},{1,8},{1,13},{1,18},{1,19},{1,20},{1,24},{1,25},{1,27},{1,30},{1,31},{1,32},{1,37},{1,39},{1,42},{1,43},{1,44},{1,48},{1,49},{1,51},{1,54},{1,55},{1,56},{1,60},{1,61},{1,63},
+	{2,0},{2,1},{2,3},{2,6},{2,7},{2,8},{2,13},{2,18},{2,19},{2,20},{2,24},{2,25},{2,27},{2,30},{2,31},{2,32},{2,37},{2,39},{2,42},{2,43},{2,44},{2,48},{2,49},{2,51},{2,54},{2,55},{2,56},{2,60},{2,61},{2,63},
+	{3,32},{3,37},{3,39},{3,42},{3,43},{3,44},{3,48},{3,49}
+	}
+	
+	local pat = somaticState.demoPatternIndex
+	local row = somaticState.demoPatternRow
+	
+	if t > 1200 then
+		local drawlogo2 = true
+		local drawlogo = true
+		local drawback = true
+		for i=1,#rowmatch do
+			dp = rowmatch[i]
+			if (dp[1] == pat) and (dp[2] == row) then
+				local witch = i%3
+				if witch == 0 then drawlogo2 = false end
+				if witch == 1 then drawlogo = false end
+				drawback = false
+				break
+			end
+		end
+		if drawlogo2 then C03_DrawSpriteStripped("F1_Logo02", 12, 74, t) end --drawSprite("F1_Logo02",12,74) end
+		if drawlogo then C03_DrawSpriteStripped("F1_Logo",12,104,t+2000) end
+		if drawback then drawSprite("F1_LogoBackdrop",0,4) end
+	end
 
 	TwinkleTick(somaticState, "starz")
 

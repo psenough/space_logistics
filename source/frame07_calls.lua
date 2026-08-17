c448ea3cd1c47501251d11ba9b634828583e36da
Filipe Cruz <psenough@gmail.com>
2026-08-17 04:27:04 +0100

    linepath on F07

--- source/frame07_calls.lua
+++ source/frame07_calls.lua
@@ -1,97 +1,4 @@
 
---[[ drawing controllable pivots, borrowed code from elias
-local pivots={{133,83},{68,99},{68,120},{230,104},{224,2}}
-local selection=-1 -- pivot selected (-1 = none, otherwise Lua 1-based index)
-local mbt=0        -- mouse button time
-local imx,imy=-1,-1 -- store initial mouse position
-
-function drawBezierCurves(t)
-
-	local mx,my,mb=mouse()
-
-	if mb then
-		mbt=mbt+1
-	else
-		mbt=0
-		selection=-1 -- reset selection
-	end
-
-	-- first click
-	if mbt==1 then
-		imx=mx
-		imy=my
-
-		-- find the selected pivot (first case)
-		selection=-1
-		for i,p in ipairs(pivots) do
-			if math.sqrt((p[1]-mx)^2+(p[2]-my)^2)<=7 then
-				selection=i
-				break
-			end
-		end
-	else
-		local distToInitial=math.sqrt((imx-mx)^2+(imy-my)^2)
-		if distToInitial<2 and mbt==30 then
-			-- if there is a pivot selected, destroy it; otherwise, create a new one.
-			if selection==-1 then
-				if mx>=0 and my>=0 and mx<=239 and my<=135 then
-					table.insert(pivots,{mx,my})
-					selection=#pivots
-				end
-			elseif #pivots>1 then
-				table.remove(pivots,selection)
-				selection=-1
-			end
-		end
-	end
-
-	-- move the selected pivot
-	if selection>-1 then
-		local omx=math.min(math.max(mx,0),239)
-		local omy=math.min(math.max(my,0),135)
-		pivots[selection][1]=omx
-		pivots[selection][2]=omy
-	end
-
-	--cls(0)
-
-	-- draw the connections between each pivot
-	for i=2,#pivots do
-		line(pivots[i][1],pivots[i][2],pivots[i-1][1],pivots[i-1][2],14)
-	end
-
-	local ttt=time()/20%quality//1
-	--print(ttt)
-
-	-- draw the curve (terrible)
-	local pre = pBezier(pivots,0)
-	for i=1,quality do
-		local t=i/quality
-		local p=pre
-		local q=pBezier(pivots,t)
-		--linew(p[1],p[2],q[1],q[2],1,.5)
-		if i<ttt then line(p[1],p[2],q[1],q[2],9) end
-		if i==ttt then line(p[1],p[2],q[1],q[2],11) end
-		pre=q
-	end
-
-	local dump="pivots={"
-
-	-- draw the pivots
-	for i,b in ipairs(pivots) do
-		circb(b[1],b[2],7,15)
-		dump=dump.."{"..b[1]..","..b[2].."},"
-	end
-	dump=dump.."}"
-	
-	-- press space to get a dump of the current changes in pivots
-	if keyp(48) then
-		trace(dump)
-	end
-
-end
---]]
-
 F7_TrailGradient = {
 	12,11,10,11
 }
@@ -145,6 +52,83 @@ function Frame07(tt)
 	
 	drawSprite("F7_Ship_01",240-226,10+math.sin(t/1200)*2)
 
+	local paths = {
+		{ st = 0, endt = 1000, pivots={{137,86,30},{127,90,20},{240,90,5}}},
+		{ st = 0, endt = 1000, pivots={{139,86,30},{129,90,20},{0,90,5}}},
+		{ st = 0, endt = 1000, pivots={{141,86,30},{131,92,20},{240,92,5}}},
+
+		{ st = 400, endt = 1400, pivots={{137,86,30},{127,90,20},{165,90,5},{187,53,5},{187,20,5},{165,0,5}}},
+		{ st = 400, endt = 1400, pivots={{139,86,30},{129,90,20},{90,90,5},{70,53,5},{70,33,5},{70,0,5}}},
+		{ st = 400, endt = 1400, pivots={{141,86,30},{131,92,20},{240,92,5}}}, 
+
+		{ st = 800, endt = 2000, pivots={{137,86,30},{127,90,20},{240,90,5}}},
+		{ st = 800, endt = 2000, pivots={{139,86,30},{129,90,20},{0,90,5}}},
+		{ st = 800, endt = 2000, pivots={{141,86,30},{131,92,20},{240,92,5}}}, 
+
+		{ st = 1200, endt = 2400, pivots={{137,86,30},{127,90,20},{127,100,5},{240,100,5}}},
+		{ st = 1200, endt = 2400, pivots={{139,86,30},{129,90,20},{129,100,5},{116,136,5}}},
+		{ st = 1200, endt = 2400, pivots={{141,86,30},{131,92,20},{131,100,5},{0,100,5}}}, 
+
+
+
+		--[[{ st = 0, endt = 2200, pivots={{130,73,30},{140,86,20},{159,103,10},{240,54,0}}},
+		{ st = 0, endt = 2200, pivots={{129,73,30},{139,86,20},{158,103,10},{201,136,0}}},
+		{ st = 0, endt = 2200, pivots={{128,73,30},{138,86,20},{157,103,10},{78,136,0}}},
+
+		{ st = 1200, endt = 3600, pivots={{131,73,30},{141,86,20},{160,103,5},{160,123,5},{160,136,0}}},
+		{ st = 1200, endt = 3600, pivots={{129,73,30},{139,86,20},{158,103,5},{158,123,5},{128,136,0}}},
+		{ st = 1200, endt = 3600, pivots={{127,73,30},{137,86,20},{156,103,5},{156,123,5},{156,136,0}}},
+
+		{ st = 2200, endt = 3600, pivots={{131,73,30},{141,86,20},{160,103,5},{180,103,5},{240,103,0}}},
+		{ st = 2200, endt = 3600, pivots={{129,73,30},{139,86,20},{158,105,5},{178,105,5},{240,105,0}}},
+		{ st = 2200, endt = 3600, pivots={{127,73,30},{137,86,20},{156,103,5},{176,103,5},{240,136,0}}},
+
+		{ st = 3600, endt = 5200, pivots={{130,73,30},{140,86,20},{159,103,10},{240,54,0}}},
+		{ st = 3600, endt = 5200, pivots={{129,73,30},{139,86,20},{158,103,10},{213,136,0}}},
+		{ st = 3600, endt = 5200, pivots={{128,73,30},{138,86,20},{157,103,10},{78,136,0}}},
+--]]
+	}
+
+	local linegrad = {12,11,12,10,10,10,10,10,10,10}  
+	
+	for p=1,#paths do
+		local linestart = paths[p].st
+		local lineend = paths[p].endt
+		local linepath = paths[p].pivots
+		if t > linestart and t < lineend+1000 then
+			
+			local st = lineend - linestart
+			local pt = t - linestart
+			local steps = 0
+			for i=1,#linepath-1 do
+				steps = steps + linepath[i][3]
+			end
+			
+			local cstep = (pt/st)*steps//1
+			
+			local count = 1
+			local lastx = linepath[1][1]
+			local lasty = linepath[1][2]
+			
+			for i=1,#linepath-1 do
+				local refx = (linepath[i+1][1]-linepath[i][1])/linepath[i][3]
+				local refy = (linepath[i+1][2]-linepath[i][2])/linepath[i][3]
+				for s=1,linepath[i][3] do
+					local px = linepath[i][1]+refx*s
+					local py = linepath[i][2]+refy*s
+					local c = (cstep-count)//1 
+					if c > 1 and c < #linegrad then
+						line(lastx,lasty,px,py,linegrad[c])
+					end
+					count = count + 1
+					lastx = px
+					lasty = py
+				end
+			end
+		end
+	end
+
+
 	--drawBezierCurves(t)
 
 	--line(40,107,78,107,math.random(10,11))
@@ -153,7 +137,7 @@ function Frame07(tt)
 
 	--line(150,42,172,42,math.random(10,11))
 	--line(178,42,182,42,math.random(10,11))
-
+--[[
 	local curves = {
 		--{ st = 0, pivots={{133,83},{68,99},{26,72},{-20,47}}},
 		{ st = 0, pivots={{133,83},{22,81},{79,109},{29,11},{13,-20}}},
@@ -184,10 +168,12 @@ function Frame07(tt)
 			end
 		end
 	end
+--]]
 
-	drawSprite("F7_Ship_02",200-t/50,100)
+	local shipY = 110+math.sin(t/2300)*1.5
+	drawSprite("F7_Ship_02",200-t/50,shipY)
 	UpdateParticlePool(F7_SmallShipParticles)
-	F7_AddTrailParticle(230-t/50,107)
-	F7_RenderParticles(230-t/50,107)
+	F7_AddTrailParticle(230-t/50,shipY+7)
+	F7_RenderParticles(230-t/50,shipY+7)
 
 end
